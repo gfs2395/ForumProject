b80a6490cf6a7a58348a9f5d4b112c83e9c731d6
Guillermo Fernández Sanz <guifers23@gmail.com>
2023-01-25 13:29:27 +0100

    Initial project files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from 'src/forum/Main/Main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
